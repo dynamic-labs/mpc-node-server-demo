9bf22feb044d744c63c434d7746af1c5eece11b3
Ellie Farrisi <47927981+gfarrisi@users.noreply.github.com>
2025-02-06 16:40:19 -0600

    chore: add todo comment (#18)

--- src/handlers/middleware/registerOperationHandlers.ts
+++ src/handlers/middleware/registerOperationHandlers.ts
@@ -1,29 +1,29 @@
-import path from 'path';
-import { Express } from 'express';
+import path from "path";
+import { Express } from "express";
 
-import { middleware as OpenApiValidator } from 'express-openapi-validator';
+import { middleware as OpenApiValidator } from "express-openapi-validator";
 
-import 'dotenv/config';
-import { EacType, PartialEacType } from '../../generated';
-import { evervaultDecrypt } from '../../services/evervault';
-import { verifyJWT } from '../../services/jwt';
+import "dotenv/config";
+import { EacType, PartialEacType } from "../../generated";
+import { evervaultDecrypt } from "../../services/evervault";
+import { verifyJWT } from "../../services/jwt";
 
-const isDeployedEnv = process.env.NODE_ENV === 'production';
+const isDeployedEnv = process.env.NODE_ENV === "production";
 
 export const registerOperationHandlers = (app: Express) => {
   // EWC and EAC parsing middleware
   //TODO: make sure i add all the routes that need this encryption middleware
   app.use(
     [
-      '/api/v1/actions/CreateWalletAccount',
-      '/api/v1/actions/ExportWalletAccount',
-      '/api/v1/actions/SignMessage',
-      '/api/v1/actions/RefreshShares',
-      '/api/v1/actions/CreateRoom',
-      '/api/v1/actions/ImportPrivateKey',
+      "/api/v1/actions/CreateWalletAccount",
+      "/api/v1/actions/ExportWalletAccount",
+      "/api/v1/actions/SignMessage",
+      "/api/v1/actions/RefreshShares",
+      "/api/v1/actions/CreateRoom",
+      "/api/v1/actions/ImportPrivateKey",
     ],
     async (req, _res, next) => {
-      console.log('---original route---', req.originalUrl);
+      console.log("---original route---", req.originalUrl);
       if (req.body.serverEacs) {
         if (isDeployedEnv) {
           // encrypted account credential (EAC) should be decrypted on ingress
@@ -42,36 +42,37 @@ export const registerOperationHandlers = (app: Express) => {
               return {
                 ...parsedEac,
                 serverKeygenInitResult:
-                  typeof parsedEac.serverKeygenInitResult === 'string'
+                  typeof parsedEac.serverKeygenInitResult === "string"
                     ? parsedEac.serverKeygenInitResult
                     : JSON.stringify(parsedEac.serverKeygenInitResult),
               } as PartialEacType;
-            }),
+            })
           );
           req.body.serverEacs = serverEacParsed;
-          console.log('Processed serverEacs:', req.body.serverEacs);
+          console.log("Processed serverEacs:", req.body.serverEacs);
         }
       }
-      console.log('next ');
+      console.log("next ");
       next();
-    },
+    }
   );
 
   app.use(
-    '/api',
+    "/api",
     OpenApiValidator({
-      apiSpec: path.join(__dirname, '../../generated/openapi/api@v1.yaml'),
+      apiSpec: path.join(__dirname, "../../generated/openapi/api@v1.yaml"),
       validateRequests: {
         allowUnknownQueryParameters: false,
         coerceTypes: false,
-        removeAdditional: 'failing',
+        removeAdditional: "failing",
       },
       validateResponses: true,
-      operationHandlers: path.join(__dirname, '../../handlers/v1'),
-    }),
+      operationHandlers: path.join(__dirname, "../../handlers/v1"),
+    })
   );
 
-  app.use(['/api/v1/actions/CreateWalletAccount'], async (req, res, next) => {
+  //todo: add other actions that need this middleware
+  app.use(["/api/v1/actions/CreateWalletAccount"], async (req, res, next) => {
     const { jwt, eac } = req.body;
     let environmentId, userId;
     if (eac) {
@@ -80,8 +81,8 @@ export const registerOperationHandlers = (app: Express) => {
 
     if (!environmentId || !userId) {
       return res.status(403).send({
-        error_code: 'jwt_invalid',
-        error_message: 'The JWT could not be verified',
+        error_code: "jwt_invalid",
+        error_message: "The JWT could not be verified",
       });
     }
     const { isVerified } = await verifyJWT({
@@ -93,8 +94,8 @@ export const registerOperationHandlers = (app: Express) => {
     // If the JWT cannot be not verified, return unauthorized
     if (!isVerified) {
       return res.status(403).send({
-        error_code: 'jwt_invalid',
-        error_message: 'The JWT could not be verified',
+        error_code: "jwt_invalid",
+        error_message: "The JWT could not be verified",
       });
     }
     next();
