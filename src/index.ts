96793b24ed8f9d825b8e09fbce786284c2dbaf71
Ellie Farrisi <>
2025-04-01 13:21:19 -0500

    chore: add env port

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import express from 'express';
-import { registerMiddleware } from './handlers/middleware/registerMiddleware';
-import logger from './logger';
+import express from "express";
+import { registerMiddleware } from "./handlers/middleware/registerMiddleware";
+import logger from "./logger";
 
 const app = express();
 
 registerMiddleware(app);
 
-if (process.env.NODE_ENV !== 'test') {
-  const server = app.listen(8010, () => {
-    logger.info('Express started on port 8010');
+const port = process.env.PORT || 8010; // Use Heroku's PORT or default to 8010
+
+if (process.env.NODE_ENV !== "test") {
+  const server = app.listen(port, () => {
+    logger.info("Express started on port 8010");
   });
   server.setTimeout(10000); // 10 seconds
 }
