4e280758bdf24697b4656d8f71b445c092dbb2ed
Ellie Farrisi <>
2025-04-08 15:52:27 -0500

    chore: fix file structure

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,10 @@ const app = express();
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/loadforge.txt", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "loadforge.txt"));
+});
+
 registerMiddleware(app);
 
 const port = process.env.PORT || 8010; // Use Heroku's PORT or default to 8010
